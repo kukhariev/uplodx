1ec0dacdb8b72851b0aa7bc9017bd62d7fea9dbf
q_h <q_h@inbox.ru>
2020-09-30 00:23:50 +0300

    chore: update fastify example

--- examples/fastify.ts
+++ examples/fastify.ts
@@ -1,18 +1,23 @@
-import * as Fastify from 'fastify';
+import fastify from 'fastify';
 import { Uploadx } from '../src';
 import { join } from 'path';
 
-const fastify = Fastify({ logger: true });
+const server = fastify({ logger: true });
 const uploadx = new Uploadx({ directory: 'files' });
 uploadx.on('completed', ({ name, originalName }) =>
-  fastify.log.info(
+  server.log.info(
     `upload complete, path: ${join('files', name)}, original filename: ${originalName}`
   )
 );
 
-fastify.use('/upload/files', uploadx.handle);
-
-fastify.listen(3003, (err, address) => {
-  if (err) throw err;
-  fastify.log.info(`server listening on ${address}`);
-});
+// eslint-disable-next-line promise/catch-or-return
+server.register(require('fastify-express')).then(
+  () => {
+    server.use('/files', uploadx.handle);
+    return server.listen(3003, (err, address) => {
+      if (err) throw err;
+      server.log.info(`server listening on ${address}`);
+    });
+  },
+  e => console.error(e)
+);
